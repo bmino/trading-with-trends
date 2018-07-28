333d3b6b8c97c2f729392027cc4ab15cfb963d33
Unknown <brandonmino@bellsouth.net>
2018-07-28 17:15:02 -0400

    Remove dotenv Library

---
+++ config/application.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Binance Api Configuration
+    BINANCE_API_KEY: 'wZx5Ua95p6U5lkGxrg32rY1eDpEsx3HsXmn8q5h1k0nwyM9iSNwJQsB2Ll3rEnB8',
+    BINANCE_API_SECRET: 'mMh9bx4xQ2b8qEhsIsmZbtVFEgHSzKr6aBMUJ9QHXBQIZxhS6OQXrZsC7DxPpshb',
+    
+    // Mailer Api Configuration
+    MAIL_AUTH_CLIENT_USER: 'tradingwithtrends@gmail.com',
+    MAIL_AUTH_CLIENT_ID: '509286419735-1240hptu76h0gr9fhmqg78gg71lueb65.apps.googleusercontent.com',
+    MAIL_AUTH_CLIENT_SECRET: 'ZSkO9Lp1LlkIlbLr3gd4615H',
+    MAIL_AUTH_CLIENT_REFRESH: '1/S6tDGcSs7jy_cRDPLLe0v_Gp6VS_J-wBn0UhVLZEGE95OOTRG5DBPpNcKVUYsORa',
+    
+    // Mailer Configuration
+    NOTIFICATION_EMAIL_ADDRESS: 'username@host.com'
+};
